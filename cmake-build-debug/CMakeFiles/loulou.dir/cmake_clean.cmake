47f18910759154d68cfd0f72e0a8d8dd79ac03c2
ThomasRebaudet <thomas.rebaudet@robolyon.com>
2020-05-16 18:56:38 +0200

    test
    test

---
+++ cmake-build-debug/CMakeFiles/loulou.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/loulou.dir/exercice1.c.obj"
+  "CMakeFiles/loulou.dir/main.c.obj"
+  "libloulou.dll.a"
+  "loulou.exe"
+  "loulou.exe.manifest"
+  "loulou.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/loulou.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "exercice1.h"
+int main() {
+    Barillet* chargeur = malloc(6* sizeof(Barillet));
+
+
+
+
+
+    return 0;
+}
